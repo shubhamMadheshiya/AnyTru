847b47e947fa2c92133e6082951a5537bbb24afb
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-03 05:52:36 +0530

    adding important middle ware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const config = require('./config/config')
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const PORT = config.port || 6000;
@@ -10,6 +11,15 @@ const PORT = config.port || 6000;
 // Connect to MongoDB
 connectDB();
 
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
 
 mongoose.connection.once('open', () => {
 	console.log('Connected to MongoDB');
