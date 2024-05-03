ff7fb90664809a84b36e709bf92df57be69b65c3
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-03 06:27:27 +0530

    adding path module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const config = require('./config/config');
