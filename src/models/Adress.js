ebe0f4b429606410be0f5404ae29843902cad1e1
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-13 21:05:17 +0530

    model address

--- src/models/Adress.js
+++ src/models/Adress.js
@@ -1,16 +1,43 @@
-const mongoose = require('mongoose');
+const Mongoose = require('mongoose');
+const { Schema } = Mongoose;
 
-const addressSchema = new mongoose.Schema({
-	adressType: {
+// Address Schema
+const AddressSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	addressType: {
 		type: String,
 		enum: ['Home', 'Office', 'Hotel', 'Other'],
 		default: 'Home',
 		required: true
 	},
-	pincode: { type: Number, required: true },
-	houseDetail: { type: String, required: true }
+	address: {
+		type: String
+	},
+	city: {
+		type: String
+	},
+	state: {
+		type: String
+	},
+	country: {
+		type: String,
+		default: 'India'
+	},
+	pinCode: {
+		type: String
+	},
+	isDefault: {
+		type: Boolean,
+		default: false
+	},
+	updated: Date,
+	created: {
+		type: Date,
+		default: Date.now
+	}
 });
 
-const Address = mongoose.model('Address', addressSchema);
-
-module.exports = Address;
+module.exports = Mongoose.model('Address', AddressSchema);
