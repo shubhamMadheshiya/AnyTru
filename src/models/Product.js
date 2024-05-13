931fdeda4fc2e8a5abb76de51df2ff399dfd3c46
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-13 20:25:54 +0530

    product model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,5 @@
 const Mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator');
+const slug = require('mongoose-slug-updater');
 const { Schema } = Mongoose;
 
 const options = {
@@ -35,7 +35,7 @@ const ProductSchema = new Schema({
 		trim: true
 	},
 	quantity: {
-		type: Number
+		type: Number,
 	},
 	price: {
 		type: Number
@@ -58,27 +58,40 @@ const ProductSchema = new Schema({
 		ref: 'User',
 		default: null
 	},
-	tags: {
-		type: [String],
-		required: true,
-		validate: {
-			validator: function (v) {
-				return v.length >= 5;
-			},
-			message: 'At least 5 tags are required'
-		}
-	},
-	likes: {
-		type: [Schema.Types.ObjectId],
+	   tags:[{ 
+		type:String,
+        // Change tags to be an array of strings
+        // validate: {
+        //     validator: function (v) {
+        //         return v.length >= 5;
+        //     },
+        //     message: 'At least 5 tags are required'
+        // }
+	}]
+    ,
+	likes: [{
+		type: Schema.Types.ObjectId,
 		ref: 'User',
-		default: []
-	},
+	}],
 	link: String,
+	public:{
+		type:Boolean,
+		default:true,
+	},
 	updated: Date,
 	created: {
 		type: Date,
 		default: Date.now
 	}
+});
+
+// Error handling for slug generation
+ProductSchema.post('save', function(error, doc, next) {
+    if (error.name === 'MongoError' && error.code === 11000) {
+        next(new Error('Slug must be unique.'));
+    } else {
+        next(error);
+    }
 });
 
 module.exports = Mongoose.model('Product', ProductSchema);
