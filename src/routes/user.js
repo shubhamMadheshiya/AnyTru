1e1fdb59c8c5565259b7610517be124119d7612c
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-15 15:41:43 +0530

    modify user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,95 @@
 const express = require('express');
 const router = express.Router();
-
+const role = require('../middleware/role');
 const userController = require('../controllers/user.controller');
 const auth = require('../middleware/auth')
 const {authorizeRole} = require('../middleware/authorizeRole')
 const { ROLES } = require('../constants/index');
+const User = require('../models/User');
 const {checkUserId}= require('../middleware/checkUserId')
 
 
+// search users api
+router.get('/search', auth,  async (req, res) => {
+  try {
+    const { search } = req.query;
 
+    const regex = new RegExp(search, 'i');
+
+    const users = await User.find(
+			{
+				$or: [
+					{ firstName: { $regex: regex } },
+					{ lastName: { $regex: regex } },
+					{ email: { $regex: regex } },
+					{ userId: { $regex: regex } }
+				]
+			},
+			{ password: 0, _id: 0 }
+		)
+        // .populate('user', 'name'); merchant
+
+    res.status(200).json({
+      users
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+
+// fetch users api
+router.get('/', auth, async (req, res) => {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+
+    const users = await User.find({}, { password: 0, _id: 0 })
+      .sort('-created')
+    //   .populate('merchant', 'name')
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+
+    const count = await User.countDocuments();
+
+    res.status(200).json({
+      users,
+      totalPages: Math.ceil(count / limit),
+      currentPage: Number(page),
+      count
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+//get merchant
+router.get('/me', auth, async (req, res) => {
+	try {
+		const user = req.user._id;
+		const userDoc = await User.findById(user, { password: 0 }).populate({
+			path: 'merchant',
+			model: 'Merchant',
+			populate: {
+				path: 'brand',
+				model: 'Brand'
+			}
+		});
+
+		res.status(200).json({
+			user: userDoc
+		});
+	} catch (error) {
+		res.status(400).json({
+			error: 'Your request could not be processed. Please try again.'
+		});
+	}
+});
 
 // POST request to create a new user
 router.post('/', auth, authorizeRole(ROLES.Admin), userController.createUser);
