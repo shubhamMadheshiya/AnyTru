35c41316c4bc245e948e4c62c3d0b7a63e954457
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-15 23:14:38 +0530

    vendor model

---
+++ src/models/Vendor.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const vendorSchema = new Schema({
+	name: {
+		type: String,
+		unique: true,
+		trim: true
+	},
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	merchant: {
+		type: Schema.Types.ObjectId,
+		ref: 'Merchant',
+		required: true
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 5,
+		default: 1
+	},
+	image: {
+		data: Buffer,
+		contentType: String
+	},
+	description: {
+		type: String,
+		trim: true
+	},
+	isActive: {
+		type: Boolean,
+		default: true
+	},
+	updated: Date,
+	created: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+const Vendor = mongoose.model('Vendor', vendorSchema);
+
+module.exports = Vendor;
