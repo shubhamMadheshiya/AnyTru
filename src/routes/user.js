69ac48d79f3c08417975efadabfbe9e7acf12f09
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-16 21:15:27 +0530

    user model

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 const role = require('../middleware/role');
 const userController = require('../controllers/user.controller');
-const auth = require('../middleware/auth')
-const {authorizeRole} = require('../middleware/authorizeRole')
-const { ROLES } = require('../constants/index');
+const auth = require('../middleware/auth');
+const { authorizeRole } = require('../middleware/authorizeRole');
+const { ROLES, MERCHANT_STATUS } = require('../constants/index');
 const User = require('../models/User');
-const {checkUserId}= require('../middleware/checkUserId')
-
+const Vendor = require('../models/Vendor');
+const Merchant = require('../models/merchant');
+const { checkUserId } = require('../middleware/checkUserId');
 
 // search users api
-router.get('/search', auth,  async (req, res) => {
-  try {
-    const { search } = req.query;
+router.get('/search', auth, async (req, res) => {
+	try {
+		const { search } = req.query;
 
-    const regex = new RegExp(search, 'i');
+		const regex = new RegExp(search, 'i');
 
-    const users = await User.find(
+		const users = await User.find(
 			{
 				$or: [
 					{ firstName: { $regex: regex } },
@@ -26,59 +27,74 @@ router.get('/search', auth,  async (req, res) => {
 				]
 			},
 			{ password: 0, _id: 0 }
-		)
-        // .populate('user', 'name'); merchant
+		);
+		// .populate('user', 'name'); merchant
 
-    res.status(200).json({
-      users
-    });
-  } catch (error) {
-    console.log(error)
-    res.status(400).json({
-      error: 'Your request could not be processed. Please try again.'
-    });
-  }
+		res.status(200).json({
+			users
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			error: 'Your request could not be processed. Please try again.'
+		});
+	}
 });
-
 
 // fetch users api
 router.get('/', auth, async (req, res) => {
-  try {
-    const { page = 1, limit = 10 } = req.query;
+	try {
+		const { page = 1, limit = 10 } = req.query;
 
-    const users = await User.find({}, { password: 0, _id: 0 })
-      .sort('-created')
-    //   .populate('merchant', 'name')
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .exec();
+		const users = await User.find({}, { password: 0, _id: 0 })
+			.sort('-created')
+			//   .populate('merchant', 'name')
+			.limit(limit * 1)
+			.skip((page - 1) * limit)
+			.exec();
 
-    const count = await User.countDocuments();
+		const count = await User.countDocuments();
 
-    res.status(200).json({
-      users,
-      totalPages: Math.ceil(count / limit),
-      currentPage: Number(page),
-      count
-    });
-  } catch (error) {
-    res.status(400).json({
-      error: 'Your request could not be processed. Please try again.'
-    });
-  }
+		res.status(200).json({
+			users,
+			totalPages: Math.ceil(count / limit),
+			currentPage: Number(page),
+			count
+		});
+	} catch (error) {
+		res.status(400).json({
+			error: 'Your request could not be processed. Please try again.'
+		});
+	}
+});
+// get merchant status
+
+router.get('/vendor/status', auth, async (req, res) => {
+	try {
+		const userId = req.user._id;
+		const findVendorReq = await Merchant.findOne({ user: userId }, { status: 1, _id: 0 });
+		if (!findVendorReq) {
+			return res.status(400).json({ error: 'No request found' });
+		}
+		return res.status(200).json(findVendorReq);
+	} catch (error) {
+		res.status(400).json({
+			error: 'Your request could not be processed. Please try again.'
+		});
+	}
 });
 
 //get merchant
-router.get('/me', auth, async (req, res) => {
+router.get('/vendor', auth, role.check(ROLES.Merchant), async (req, res) => {
 	try {
-		const user = req.user._id;
-		const userDoc = await User.findById(user, { password: 0 }).populate({
-			path: 'merchant',
-			model: 'Merchant',
-			populate: {
-				path: 'brand',
-				model: 'Brand'
-			}
+		const userId = req.user._id;
+		const userDoc = await User.findById(userId, { password: 0 }).populate({
+			path: 'vendor',
+			model: 'Vendor'
+			// populate: {
+			// 	path: 'brand',
+			// 	model: 'Brand'
+			// }
 		});
 
 		res.status(200).json({
@@ -98,7 +114,7 @@ router.post('/', auth, authorizeRole(ROLES.Admin), userController.createUser);
 // router.get('/',auth, userController.createUser);
 
 // GET request to get user by userId
-router.get('/:userId', auth , userController.getUserById);
+router.get('/:userId', auth, userController.getUserById);
 
 // PUT request to update user by userId
 router.patch('/:userId', auth, authorizeRole(ROLES.User), userController.updateUser);
@@ -106,12 +122,8 @@ router.patch('/:userId', auth, authorizeRole(ROLES.User), userController.updateU
 // DELETE request to delete user by userId
 router.delete('/:userId', auth, authorizeRole(ROLES.User), userController.deleteUser);
 
-
-
-
-
 // POST request to follow a user
-router.post('/:userId/follow',auth, authorizeRole(ROLES.User), userController.followUser);
+router.post('/:userId/follow', auth, authorizeRole(ROLES.User), userController.followUser);
 
 // POST request to unfollow a user
 router.post('/:userId/unfollow', auth, authorizeRole(ROLES.User), userController.unfollowUser);
@@ -125,12 +137,10 @@ router.post('/rate', userController.rateUser);
 // POST request to switch account type
 router.post('/switch-account', userController.switchAccountType);
 
-
 // GET request to get followers of a user by userId
-router.get('/:userId/followers', auth,  userController.getFollowers);
+router.get('/:userId/followers', auth, userController.getFollowers);
 
 // GET request to get users followed by a user by userId
-router.get('/:userId/following', auth,  userController.getFollowing);
-
+router.get('/:userId/following', auth, userController.getFollowing);
 
 module.exports = router;
