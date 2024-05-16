1b2b9d477ff47f779da2120c034db0b8c62db477
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-16 21:47:40 +0530

    server.js

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.use('/aboutUs', require('./src/routes/aboutUs'));
 app.use('/t&c', require('./src/routes/t&c'));
 app.use('/contact', require('./src/routes/contact'));
 app.use('/merchant', require('./src/routes/merchant'));
+app.use('/vendor', require('./src/routes/vendor'));
 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
