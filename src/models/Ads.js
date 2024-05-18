4416ae0dcf455972e2efb9d7570306967b3179f1
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-18 22:49:12 +0530

    modyfy ads model

--- src/models/Ads.js
+++ src/models/Ads.js
@@ -1,44 +1,60 @@
 const mongoose = require('mongoose');
 
-const adsSchema = new mongoose.Schema({
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	product: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Product',
-		required: true
-	},
-	address: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Address',
-		required: true
-	},
-	pricePerProduct: {
-		type: Number,
-		required: true
-	},
-	quantity: {
-		type: Number,
-		required: true
-	},
-	vendors: [
-		{
+const adsSchema = new mongoose.Schema(
+	{
+		user: {
 			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Vendor'
-		}
-	],
-    isActive:{
-        type:Boolean,
-        default:true
-    },
-    category:[{
-        type:String,
-        default:"category"
-    }]
-},{timestamps:true});
+			ref: 'User',
+			required: true
+		},
+		product: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Product',
+			required: true
+		},
+		address: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Address',
+			required: true
+		},
+		pricePerProduct: {
+			type: Number,
+			required: true
+		},
+		quantity: {
+			type: Number,
+			required: true
+		},
+		vendors: [
+			{
+				vendor: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'Vendor'
+				},
+				pricePerProduct: {
+					type: Number
+				},
+				dispatchDay: {
+					type: Number
+				},
+				remark: {
+					type: String
+				}
+			}
+		],
+		isActive: {
+			type: Boolean,
+			default: true
+		},
+		category: [
+			{
+				type: String,
+				default: 'category'
+			}
+		]
+	},
+	{ timestamps: true }
+);
 
 const Ads = mongoose.model('Ad', adsSchema);
 
