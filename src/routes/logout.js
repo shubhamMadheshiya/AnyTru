7b8e366b663e5f2d83619f594b0ea2c4a8ac753e
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-24 00:24:24 +0530

    logout

--- src/routes/logout.js
+++ src/routes/logout.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	req.logout(function (err) {
-		if (err) {
-			return next(err);
-		}
+	// req.logout(function (err) {
+	// 	if (err) {
+	// 		return next(err);
+	// 	}
+	
+	// });
+	console.log("logout")
 		res.redirect('http://localhost:3000');
-	});
 });
 
 module.exports = router;
