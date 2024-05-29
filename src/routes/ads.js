dfd0e33464faa2c11c6c1f3ebaee89fc44b5004a
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-05-30 03:55:35 +0530

    add routes

--- src/routes/ads.js
+++ src/routes/ads.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const Ads = require('../models/Ads');
 const Address = require('../models/Adress');
 const User = require('../models/User');
-const Product = require('../models/product');
+const Product = require('../models/Product');
 const { ROLES } = require('../constants');
 const auth = require('../middleware/auth');
 const role = require('../middleware/role');
 const Vendor = require('../models/Vendor');
 
 // Create an Ad
-router.post('/:productId', auth, async (req, res) => {
+router.post('/add/:productId', auth, async (req, res) => {
 	try {
 		const userId = req.user._id;
 		const productId = req.params.productId;
@@ -38,18 +38,19 @@ router.post('/:productId', auth, async (req, res) => {
 			return res.status(404).json({ error: 'Product not found' });
 		}
 
-		const existingAd = await Ads.findOne({ user: userId, product: productId });
+		// const existingAd = await Ads.findOne({ user: userId, product: productId });
 
-		if (existingAd) {
-			return res.status(400).json({ error: 'You already have an ad for this product' }); // Changed status code to 400 Bad Request
-		}
+		// if (existingAd) {
+		// 	return res.status(400).json({ error: 'You already have an ad for this product' }); // Changed status code to 400 Bad Request
+		// }
 
 		const ads = new Ads({
 			user: user._id,
 			product: product._id,
 			address: address._id,
 			pricePerProduct,
-			quantity
+			quantity,
+			category: product.category
 		});
 		const savedAd = await ads.save();
 		res.status(201).json({
@@ -63,42 +64,75 @@ router.post('/:productId', auth, async (req, res) => {
 	}
 });
 
-router.get('/', auth, role.check(ROLES.Merchant, ROLES.Admin), async (req, res) => {
-	const { page = 1, limit = 10 } = req.query;
-	const vendorId = req.user.vendor;
-	console.log(vendorId);
+// Get list of all products with filter
+router.get('/list', auth, role.check(ROLES.Merchant, ROLES.Admin), async (req, res) => {
+    const { page = 1, limit = 10, pricePerProduct, category, quantity, isActive, sortField, sortOrder } = req.query;
+    const vendorId = req.user.vendor;
+    const userRole = req.user.role;
 
-	try {
-		let adsData;
+    let filter = {};
+    let sort = {};
 
-		if (vendorId) {
-			const vendorAds = await Vendor.findById(vendorId);
-			adsData = await Ads.find({ isActive: true, _id: { $nin: [...vendorAds.ads, ...vendorAds.rejAds] } })
-				.populate('user')
-				.populate('address')
-				.populate('product')
-				.sort('-createdAt') // Changed from '-created' to '-createdAt'
-				.limit(limit * 1)
-				.skip((page - 1) * limit)
-				.exec();
-		} else {
-			adsData = await Ads.find()
-				.populate('user')
-				.populate('address')
-				.populate('product')
-				.sort('-createdAt') // Changed from '-created' to '-createdAt'
-				.limit(limit * 1)
-				.skip((page - 1) * limit)
-				.exec();
-		}
+    if (pricePerProduct) {
+        filter.pricePerProduct = { $lte: pricePerProduct };
+    }
 
-		const count = await Ads.countDocuments();
-		res.json({ adsData, totalPages: Math.ceil(count / limit), currentPage: Number(page), count });
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: 'Internal server error' });
-	}
+    if (category) {
+        filter.category = { $in: category.split(',') };
+    }
+
+    if (quantity) {
+        filter.quantity = { $gte: quantity };
+    }
+
+    if (sortField && sortOrder) {
+        sort[sortField] = sortOrder === 'asc' ? 1 : -1;
+    } else {
+        sort = { createdAt: -1 }; // Default sorting by createdAt in descending order
+    }
+
+    // Filtering by isActive
+    if (isActive !== undefined) {
+        if (isActive === 'false' && userRole !== ROLES.Admin) {
+            return res.status(403).json({ error: 'You are not authorized to view inactive ads' });
+        }
+        filter.isActive = isActive === 'true';
+    } else if (userRole !== ROLES.Admin) {
+        filter.isActive = true; // Default to only active ads for non-admin users
+    }
+
+    try {
+        let adsData;
+
+        if (vendorId) {
+            const vendorAds = await Vendor.findById(vendorId);
+            adsData = await Ads.find({ ...filter, _id: { $nin: [...vendorAds.ads, ...vendorAds.rejAds] } })
+                .populate('user')
+                .populate('address')
+                .populate('product')
+                .sort(sort)
+                .limit(limit * 1)
+                .skip((page - 1) * limit)
+                .exec();
+        } else {
+            adsData = await Ads.find(filter)
+                .populate('user')
+                .populate('address')
+                .populate('product')
+                .sort(sort)
+                .limit(limit * 1)
+                .skip((page - 1) * limit)
+                .exec();
+        }
+
+        const count = await Ads.countDocuments(filter);
+        res.json({ adsData, totalPages: Math.ceil(count / limit), currentPage: Number(page), count });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
+
 
 
 // Get a Specific Ad by ID
@@ -146,10 +180,10 @@ router.delete('/:id', auth, async (req, res) => {
 // accept ads by vendor
 router.post('/:adId/accept', auth, role.check(ROLES.Merchant), async (req, res) => {
 	try {
-      const {pricePerProduct, dispatchDay, remark} = req.body;
-	  if(!pricePerProduct || !dispatchDay ||!remark){
-		return res.status(401).json({ error: 'All fiels are required' });
-	  }
+		const { pricePerProduct, dispatchDay, remark } = req.body;
+		if (!pricePerProduct || !dispatchDay || !remark) {
+			return res.status(401).json({ error: 'All fiels are required' });
+		}
 		const adId = req.params.adId;
 		const vendorId = req.user.vendor;
 
@@ -175,7 +209,7 @@ router.post('/:adId/accept', auth, role.check(ROLES.Merchant), async (req, res) 
 			pricePerProduct,
 			dispatchDay,
 			remark,
-			vendor:vendorId
+			vendor: vendorId
 		};
 		// Add the vendor to the ad's vendors array
 		ad.vendors.push(vendorDetails);
@@ -195,11 +229,10 @@ router.post('/:adId/accept', auth, role.check(ROLES.Merchant), async (req, res) 
 // accept ads by vendor
 router.put('/:adId/cancel', auth, role.check(ROLES.Merchant), async (req, res) => {
 	try {
-     
 		const adId = req.params.adId;
 		const vendorId = req.user.vendor;
 
-		console.log(vendorId)
+		console.log(vendorId);
 
 		// Check if the ad exists
 		const ad = await Ads.findById(adId);
@@ -219,7 +252,6 @@ router.put('/:adId/cancel', auth, role.check(ROLES.Merchant), async (req, res) =
 			return res.status(400).json({ error: 'Vendor yet not accept the ad' });
 		}
 
-	
 		// Add the vendor to the ad's vendors array
 		ad.vendors.pull(vendorId);
 		vendor.ads.pull(adId);
@@ -274,35 +306,33 @@ router.post('/:adId/reject', auth, role.check(ROLES.Merchant), async (req, res) 
 	}
 });
 
-
 // Deactivate an Ad
 router.put('/:adId/active', auth, async (req, res) => {
-    try {
-        const adId = req.params.adId;
-        const userId = req.user._id;
+	try {
+		const adId = req.params.adId;
+		const userId = req.user._id;
 		const isActive = req.body.isActive;
 
-        // Check if the ad exists
-        const ad = await Ads.findById(adId);
-        if (!ad) {
-            return res.status(404).json({ error: 'Ad not found' });
-        }
+		// Check if the ad exists
+		const ad = await Ads.findById(adId);
+		if (!ad) {
+			return res.status(404).json({ error: 'Ad not found' });
+		}
 
-        // Check if the user is either the admin or the owner of the ad
-        if (req.user.role === ROLES.Admin || ad.user.toString() === userId.toString()) {
-            // Update the ad to deactivate it
-            ad.isActive = isActive;
-            await ad.save();
+		// Check if the user is either the admin or the owner of the ad
+		if (req.user.role === ROLES.Admin || ad.user.toString() === userId.toString()) {
+			// Update the ad to deactivate it
+			ad.isActive = isActive;
+			await ad.save();
 
-            return res.json({ success: true, message: 'Ad deactivated successfully' });
-        } else {
-            return res.status(403).json({ error: 'You are not authorized to deactivate this ad' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
+			return res.json({ success: true, message: 'Ad deactivated successfully' });
+		} else {
+			return res.status(403).json({ error: 'You are not authorized to deactivate this ad' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal server error' });
+	}
 });
-
 
 module.exports = router;
