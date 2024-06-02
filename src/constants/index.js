5ad38d78e501a53a7eb712aaa4989abe3aae0c4b
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-06-02 23:52:36 +0530

    add url constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
+const keys = require('../config/keys');
+
 exports.ROLES = {
-  Admin: 'ADMIN',
-  User: 'USER',
-  Merchant: 'MERCHANT'
+	Admin: 'ADMIN',
+	User: 'USER',
+	Merchant: 'MERCHANT'
 };
 exports.CATEGORIES = {
 	Furniture: 'Furniture',
@@ -17,37 +19,45 @@ exports.CATEGORIES = {
 exports.ADMIN_EMAILS = ['Anytruofficial@gmail.com'];
 
 exports.MERCHANT_STATUS = {
-  Rejected: 'Rejected',
-  Approved: 'Approved',
-  Waiting_Approval: 'Waiting Approval'
+	Rejected: 'Rejected',
+	Approved: 'Approved',
+	Waiting_Approval: 'Waiting Approval'
 };
 
 exports.ORDER_ITEM_STATUS = {
-  Processing: 'Processing',
-  Shipped: 'Shipped',
-  Delivered: 'Delivered',
-  Cancelled: 'Cancelled',
-  Not_processed: 'Not processed'
+	Processing: 'Processing',
+	Shipped: 'Shipped',
+	Delivered: 'Delivered',
+	Cancelled: 'Cancelled',
+	Not_processed: 'Not processed'
 };
 exports.ORDER_PAYMENT_STATUS = {
-	Created:'created',
-Authorized: 'authorized',
-Captured:'captured',
-Failed: 'failed',
-Refunded:'refunded',
-Pending: 'pending',
-}
+	Created: 'created',
+	Authorized: 'authorized',
+	Captured: 'captured',
+	Failed: 'failed',
+	Refunded: 'refunded',
+	Pending: 'pending'
+};
 
 exports.REVIEW_STATUS = {
-  Rejected: 'Rejected',
-  Approved: 'Approved',
-  Waiting_Approval: 'Waiting Approval'
+	Rejected: 'Rejected',
+	Approved: 'Approved',
+	Waiting_Approval: 'Waiting Approval'
 };
 
 exports.EMAIL_PROVIDER = {
-  Email: 'Email',
-  Google: 'Google',
-  Facebook: 'Facebook'
+	Email: 'Email',
+	Google: 'Google',
+	Facebook: 'Facebook'
+};
+
+exports.ENDPOINT = {
+	Product: '/product/', //approveProduct / likes
+	UserProfile: '/user/', //follow rating
+	Ads: '/ads/', //ads bid
+	Order: '/order/', //order
+	Message: '/message/' //message
 };
 
 exports.JWT_COOKIE = 'x-jwt-cookie';
